e3d5a8434637b37eab711f4e6107090f7c4fabc5
shamshadlive <muhammadshamshad365@gmail.com>
2023-07-27 23:18:07 +0530

    settings

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-#updated
+#new updated
 ALLOWED_HOSTS = ['*']
 
 
--- static/store/js/main.js
+++ static/store/js/main.js
@@ -2,15 +2,46 @@ $(function() {
 
 	$('.owl-1').owlCarousel({
 
-    loop:true,
+    loop:false,
     margin:0,
-    nav:true,
+    nav:false,
     items: 1,
-    smartSpeed: 1000,
+    smartSpeed: 500,
     autoplay: true,
     autoplayHoverPause: true,
-    navText: ['<span class="icon-keyboard_arrow_left">', '<span class="icon-keyboard_arrow_right">']
-	})
+    dots:false,
+    
+    navText: ['<span class="icon-keyboard_arrow_left ">', '<span class="icon-keyboard_arrow_right">']
+	});
+
+    // Owl Carousel categ
+    var owl = $(".owl-carousel-category");
+    owl.owlCarousel({
+      items: 4,
+      margin: 5,
+      loop: false,
+      nav: false,
+      responsive: {
+          0:{
+            items: 1
+          },
+          480:{
+            items: 2
+          },
+          769:{
+            items: 3
+          },
+          1100:{
+            items: 4
+          },
+          1300:{
+            items: 5
+          },
+          1400:{
+            items: 6
+          }
+      }
+    });
 
 	
 })
